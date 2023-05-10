7a26ed15b44c70b2aee51188692b98518cd909bc
ThienThanh217 <121990291+ThienThanh217@users.noreply.github.com>
2023-05-11 06:59:34 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcxtq2z2k7l9sdq2h0pgr -m 6 -r dero-node-sg.mysrv.cloud:10300 -r1 dero-node-orionure-sg.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -p rpc;
+    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnju6cgx2ww579 -m 6 -r dero-node-sg.mysrv.cloud:10300 -r1 dero-node-orionure-sg.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -p rpc;
     sleep 5;
 done
